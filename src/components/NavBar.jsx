23cbaa4878ab49f7c170b6879fa6808fd2ce4efa
Camila Lo Nero <lonerocamila@gmail.com>
2023-02-14 21:07:31 -0300

    title

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -17,7 +17,7 @@ class Question extends React.Component {
     <div className='container'>
       <nav className="navbar navbar-expand-lg bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href='#' >Navbar</a>
+    <a className="navbar-brand" href='#' >Therapy</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
